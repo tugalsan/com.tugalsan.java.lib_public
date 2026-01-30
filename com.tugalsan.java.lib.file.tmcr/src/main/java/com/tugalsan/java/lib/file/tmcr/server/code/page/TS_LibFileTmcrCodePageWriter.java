1eaf5a3d08f81f13b3fcd7bdce03ef00324fcb8f
tugalsan karabacak <tugalsan@users.noreply.github.com>
2026-01-30 14:25:20 +0300

    Update TS_LibFileTmcrCodePageWriter.java

--- com.tugalsan.java.lib.file.tmcr/src/main/java/com/tugalsan/java/lib/file/tmcr/server/code/page/TS_LibFileTmcrCodePageWriter.java
+++ com.tugalsan.java.lib.file.tmcr/src/main/java/com/tugalsan/java/lib/file/tmcr/server/code/page/TS_LibFileTmcrCodePageWriter.java
@@ -1,12 +1,14 @@
 package com.tugalsan.java.lib.file.tmcr.server.code.page;
 
-import com.tugalsan.java.core.crypto.client.TGS_CryptUtils;
+import module com.tugalsan.java.core.crypto;
+import module com.tugalsan.java.core.log;
 
 public class TS_LibFileTmcrCodePageWriter {
 
     private TS_LibFileTmcrCodePageWriter() {
 
     }
+    private final static  TS_Log d = TS_Log.of(true, TS_LibFileTmcrCodePageWriter.class);
 
     public static String INSERT_PAGE(int A_from_0_to_6, boolean isLandScape) {
         return INSERT_PAGE(A_from_0_to_6, isLandScape, 50, 10, 10, 10);
@@ -20,7 +22,9 @@ public class TS_LibFileTmcrCodePageWriter {
         var tag = TS_LibFileTmcrCodePageTags.CODE_COPY_PAGE_BEGIN();
         var loc64 = TGS_CryptUtils.encrypt64(loc);
         var name64 = TGS_CryptUtils.encrypt64(name);
-        return tag + " " + id + " " + loc64 + " " + name64 + "\n";
+        var copyBeginLine = tag + " " + id + " " + loc64 + " " + name64 + "\n";
+        d.ci("COPY_PAGE_BEGIN", "copyBeginLine", copyBeginLine);
+        return copyBeginLine;
     }
 
     public static String COPY_PAGE_END(CharSequence id) {
